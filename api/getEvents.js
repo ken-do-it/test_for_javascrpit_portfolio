176ff3086365852e6172e86c30e721433618e6e1
kijin lee <lqizhen@naver.com>
2025-05-10 22:01:30 +0900

    getEvents.js handler function change  from  ()=>{} to old version function () {}

--- api/getEvents.js
+++ api/getEvents.js
@@ -1,11 +1,30 @@
 // api/getEvents.js
 
-export default handler =async (req, res) => {
-    const API_KEY = process.env.API_KEY;
+// export default handler =async (req, res) => {
+//     const API_KEY = process.env.API_KEY;
 
-    let url = `http://openapi.seoul.go.kr:8088/${API_KEY}/json/culturalEventInfo/1/1000`;
+//     let url = `http://openapi.seoul.go.kr:8088/${API_KEY}/json/culturalEventInfo/1/1000`;
 
-     try {
+//      try {
+//     const response = await fetch(url);
+//     console.log(response)
+//     const data = await response.json();
+//     res.status(200).json(data);
+//   } catch (err) {
+//     res.status(500).json({ error: "데이터 요청 실패", detail: err });
+//   }
+// }
+
+
+
+
+export default async function handler(req, res) {
+  const API_KEY = process.env.API_KEY;
+
+
+  let url = `http://openapi.seoul.go.kr:8088/${API_KEY}/json/culturalEventInfo/1/1000`;
+
+  try {
     const response = await fetch(url);
     const data = await response.json();
     res.status(200).json(data);
@@ -13,21 +32,3 @@ export default handler =async (req, res) => {
     res.status(500).json({ error: "데이터 요청 실패", detail: err });
   }
 }
-
-
-
-
-// export default async function handler(req, res) {
-//   const API_KEY = process.env.API_KEY;
-
-
-//   let url = `http://openapi.seoul.go.kr:8088/${API_KEY}/json/culturalEventInfo/1/1000`;
-
-//   try {
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     res.status(200).json(data);
-//   } catch (err) {
-//     res.status(500).json({ error: "데이터 요청 실패", detail: err });
-//   }
-// }
