c5a94f96e54537c1a20bef2a34bf5a0fb8dd820a
kijin lee <lqizhen@naver.com>
2025-05-11 18:47:10 +0900

    filter and sort and page

--- public/main.js
+++ public/main.js
@@ -1,24 +1,73 @@
 
 
 let culturalItems = []
+let eventFromToday = []
+let pageItems = []
 let totalResults = 0
 let page = 1
-let pageSize = 10
+let pageSize = 12
 let groupSize =5
 
-console.log(culturalItems)
+
+// const filterUpComingEvents = (items) => {
+//   const today = new Date();
+//   return items.filter((item) => new Date(item.STRTDATE) >= today);
+// };
+
+// const sortStartEvents = (items) => {
+//   return items.sort((a, b) => new Date(a.STRTDATE) - new Date(b.STRTDATE));
+// };
+
+const filterUpComingEvents = (items) => {
+    const today = new Date()
+    return items.filter((item)=>new Date(item.STRTDATE) >= today)
+}
+
+const sortStartEvents = (items) => {
+    return items.sort((a,b)=>new Date(a.STRTDATE) - new Date(b.STRTDATE))
+}
+
+
+    const getPageItems = (items, page) => {
+    const start = (page - 1) * pageSize;
+    const end = page * pageSize;
+    return items.slice(start, end);
+    };
 
 const getEvent = async () => {
-    // let url = new URL(`http://openapi.seoul.go.kr:8088/${API_KEY}/json/culturalEventInfo/1/12/`)
-    // const response = await fetch(url)
-    const response = await fetch ('/api/getEvents')
+    // const start = (page - 1) * pageSize + 1;
+    // const end = page * pageSize;
+    // pageItems = eventFromToday.slice(start, end);
+    // url.searchParams.set("page",page)
+    // url.searchParams.set("pageSize",pageSize)
+
+
+
+
+    let url = new URL(`http://openapi.seoul.go.kr:8088/${API_KEY}/json/culturalEventInfo/1/1000/`)
+    const response = await fetch(url)
+
+    //⬇️ 하단 await fetch ('/api/getEvents') 배포용 
+    // const response = await fetch ('/api/getEvents')
     const data = await response.json()
+    totalResults = data.list_total_count
     console.log(data)
     culturalItems = data.culturalEventInfo.row
+
+    
+    
+   // ✅ 필터 + 정렬 + 저장
+    eventFromToday = sortStartEvents(filterUpComingEvents(culturalItems));
+
     
     renderEvent()
     renderPageHTML()
 }
+
+
+
+
+
 
 const renderEvent = () => {
         // ✅ 카드 렌더링
@@ -26,7 +75,9 @@ const renderEvent = () => {
         // const endIdx = page * pageSize;
         // const pageItems = filteredItems.slice(startIdx, endIdx);
 
-        const culturalEventHTML = culturalItems.map((eItems) =>
+        const pageItems = getPageItems(eventFromToday, page);
+
+        const culturalEventHTML = pageItems.map((eItems) =>
             `<div class="card col-lg-3 col-md-6 col-sm-12" style="width: 18rem;">
             <img src="${eItems.MAIN_IMG}" class="card-img-top" alt="이미지 없음">
             <div class="card-body">
@@ -47,13 +98,16 @@ const renderPageHTML = () => {
     let totalPage = totalResults/pageSize
     //groupPage
     let groupPage = Math.ceil(page/groupSize)
+
     //lastPage
     let lastPage = Math.ceil(groupPage*groupSize)
     if (lastPage < groupPage) {
         groupPage = lastPage 
     }
+
     // firstPage
-    let firstPage = Math.ceil(lastPage -(groupPage-1)) <=0 ? 1 :lastPage -(groupPage-1)
+    let firstPage = Math.ceil(lastPage -(groupSize-1)) <=0 ? 1 :lastPage -(groupSize-1)
+
 
     let paginationHTML = ""
     for (let i=firstPage; i<=lastPage; i++){
